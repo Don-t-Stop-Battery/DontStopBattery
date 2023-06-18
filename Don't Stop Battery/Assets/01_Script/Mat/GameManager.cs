7fb97195106da5f71b26e3f647cd542e2c306355
kainel54 <kainel0504@gmail.com>
2023-06-18 15:13:33 +0900

    스코어 완성

--- Don't Stop Battery/Assets/01_Script/Mat/GameManager.cs
+++ Don't Stop Battery/Assets/01_Script/Mat/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,15 +10,21 @@ public class GameManager : MonoBehaviour
     float energy = 100f;
     [SerializeField]
     Slider slider;
-
-    private void Start()
-    {
-        StartCoroutine("EnergyDown");
-    }
+    [SerializeField]
+    TextMeshProUGUI scoreText;
+    float decimalScore;
+    int score;
 
     private void Update()
     {
         EnergyDown();
+        Score();
+        ScoreUpdate();
+    }
+
+    private void ScoreUpdate()
+    {
+        scoreText.text = "Score : " + score;
     }
     public float Energy
     {
@@ -31,10 +38,21 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Score()
+    {
+        decimalScore += Time.deltaTime;
+        score = Mathf.RoundToInt(decimalScore)*100;
+    }
+
     private void EnergyDown()
     {
         energy-= Time.deltaTime;
         slider.value = energy;
+    }
+
+    public void AddEnergy(int eneryPoint)
+    {
+        energy += eneryPoint;
     }
 
 }
